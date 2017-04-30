92fb4244abf552337ae259483f6c20d74dd93cb1
SoWon Choi <wosu008@hanmail.net>
2017-04-30 11:45:42 +0000

    first commit

---
+++ public/javascript.js
@@ -0,0 +1,5 @@
+
+    $('document').ready(function(){$('#menu').click(function(){
+              $('html, body').animate({scrollTop:$("#myDiv").offset().top
+                    },1000);
+              })});
